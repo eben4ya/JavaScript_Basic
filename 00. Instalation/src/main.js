df67515e63d3ac7457fa8ed9433d7b9bd13ee347
Benaya Imanuela <114753283+eben4ya@users.noreply.github.com>
2023-02-10 18:47:37 +0700

    Upload Template JavaScript

---
+++ 00. Instalation/src/main.js
@@ -0,0 +1,7 @@
+console.log('Halo Benaya');
+const nama = 'Benaya Imanuela';
+/* const cetakNama = (nama) => {
+    return 'Hi, nama saya ${nama}'
+}; */
+const cetakNama = (nama) => 'Hi, nama saya ${nama}'; // namanya kok ngga masuk ?
+console.log(cetakNama(nama));
